2574d891cd042abdc2fc39ced894b03a8741ad0b
Nikolay Kolganov <nikolaykolganov7@gmail.com>
2023-12-07 23:23:00 +0300

    :2w1IAuUscHf5oh

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 
 const app = express()
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
-const start = () => {
+const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_CONNECT)
         app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
     } catch (e) {
         console.log(e);
