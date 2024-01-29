7ac8238a5bb4a0d639b9f1ff049bfcab5232fa68
Nikolay Kolganov <nikolaykolganov7@gmail.com>
2024-01-29 20:03:56 +0300

    add model main page

---
+++ client/src/pages/Main/styles.js
@@ -0,0 +1,113 @@
+import { styled } from "styled-components";
+
+export const Wrapper =styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border: 5px solid red;
+    margin: 0px auto;
+    max-width: 1500px;
+    width: 100%;
+`
+
+export const Footer = styled.div`
+border: 1px solid black;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0 auto;
+    width: 100%;
+`
+
+export const Profile = styled.div`
+    border: 1px solid black;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0 auto;
+    width: 100%;
+`
+export const AvatarCurrentUser = styled.div`
+    border: 1px solid black;
+    width: 500px;
+    height: 500px;
+    margin: 50px;
+
+`
+
+export const InfoCurrentUser = styled.div`
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    margin: 50px;
+`
+
+export const ControlRibbon = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    border: 1px solid black;
+`
+
+export const Element = styled.div`
+    width: 100%;
+    border: 1px solid black;
+`
+
+export const Gallery = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    border: 1px solid black;
+`
+
+export const RibbonFriends = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: 1px solid black;
+`
+
+export const FriendsCard = styled.div`
+    max-width: 300px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: 1px solid black;
+    padding: 20px;
+
+`
+export const RibbonFriendsAvatar = styled.div`
+    width: 100%;
+    margin: 5px;
+    border: 1px solid black;
+`
+
+export const AllUsers = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: 1px solid black;
+`
+
+export const AllUsersCard = styled.div`
+    max-width: 300px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: 1px solid black;
+    padding: 20px;
+
+`
+export const RibbonAllUsersAvatar = styled.div`
+    width: 100%;
+    margin: 5px;
+    border: 1px solid black;
+`
